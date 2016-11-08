a58a04181746ce38b430ffe6b24531f078e818ff
Ivan26ru <it-1ivan@ya.ru>
2016-11-08 18:22:42 +0300

    чистый шаблон

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script><![endif]-->
+    <title>portal site</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <!-- сброс css -->
+    <link href="css/reset.css" rel="stylesheet">
+    <!-- подключение шрифтов -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- мои стили -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+        <!-- Шапка сайта -->
+        <header class="header">
+
+        </header>
+        <!-- .header-->
+        <main class="content">
+            
+        </main>
+        <!-- .content -->
+        <footer class="footer">
+
+        </footer>
+        <!-- .footer -->
+</body>
+
+</html>
+
